e7efff24c439e1c66a4e6d27bc92ac6d407a4e37
novitasalsabila <novnovv10@gmail.com>
2025-05-03 16:02:45 +0700

    middleware, gate admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useTailwind();
+        Gate::define('admin', function($user){
+            return $user->is_admin == true;
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+Route::get('dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
@@ -17,27 +17,32 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/todo', [TodoController::class, 'index'])->name('todo.index'); //tambahkan route untuk todo
+    Route::post('/todo', [TodoController::class, 'store'])->name('todo.store');
+    Route::delete('/todo/{todo}', [TodoController::class, 'destroy'])->name('todo.destroy');
+    Route::delete('/todo/delete-all-completed', [TodoController::class, 'destroyCompleted'])
+        ->name('todo.deleteallcompleted');
+
+    Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create'); //tambahkan route untuk todo
+    Route::get('/todo/{todo}/edit', [TodoController::class, 'edit'])->name('todo.edit');
+    Route ::patch('/todo/{todo}', [TodoController::class, 'update'])->name('todo.update');
+
+    Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
+    Route::patch('/todo/{todo}/incomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
+
 });
 
-Route::get('/todo', [TodoController::class, 'index'])->name('todo.index'); //tambahkan route untuk todo
-Route::post('/todo', [TodoController::class, 'store'])->name('todo.store');
-Route::delete('/todo/{todo}', [TodoController::class, 'destroy'])->name('todo.destroy');
-Route::delete('/todo/delete-all-completed', [TodoController::class, 'destroyCompleted'])
-     ->name('todo.deleteallcompleted');
 
-Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create'); //tambahkan route untuk todo
-Route::get('/todo/{todo}/edit', [TodoController::class, 'edit'])->name('todo.edit');
-Route ::patch('/todo/{todo}', [TodoController::class, 'update'])->name('todo.update');
-
-Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
-Route::patch('/todo/{todo}/incomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
-
-Route::get('/user', [UserController::class, 'index'])->name('user.index'); //tambahkan route untuk todo
-Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
-Route::get('/user/edit', [UserController::class, 'edit'])->name('user.edit');
-
-Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
-Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
+Route::middleware(['auth', 'admin'])->group(function(){
+    Route::get('/user', [UserController::class, 'index'])->name('user.index'); //tambahkan route untuk todo
+    Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
+    Route::get('/user/edit', [UserController::class, 'edit'])->name('user.edit');
+    
+    Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
+    Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
+    
+});
 
 
 require __DIR__.'/auth.php';
