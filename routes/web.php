0ba5fd06732bbd97c8d72fbe2e7e083054e7d02f
novitasalsabila <novnovv10@gmail.com>
2025-05-24 15:38:36 +0700

    mailtrap

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth', 'verified')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -48,7 +48,7 @@ Route::middleware(['auth', 'admin'])->group(function(){
 
 
 Route::middleware('auth')->group(function () {
-    Route::resource('category', CategoryController::class)->except(['show']);
+    // Route::resource('category', CategoryController::class)->except(['show']);
     Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
     Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create');
     Route::post('/category', [CategoryController::class, 'store'])->name('category.store');
---
+++ tugas10.md
@@ -0,0 +1,11 @@
+## Browser Register
+![alt text](<Screenshot/tugas10/Browser Register.png>)
+
+## Browser Verufy Email
+![alt text](<Screenshot/tugas10/Browser Verify Email.png>)
+
+## Browser Mailtrap
+![alt text](<Screenshot/tugas10/Browser Mailtrap.png>)
+
+## Browser Dashboard
+![alt text](<Screenshot/tugas10/Browser Dashboard.png>)
